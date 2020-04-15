3c9c5c8ff0d3bcc573d112fd59a73112ff691e4c
Mateusz Zdanowicz <zdanowicz.mateusz@gmail.com>
2020-04-15 20:06:22 +0200

    Create CommandResolver with PLACE command handling

---
+++ lib/turtle_app/command_resolver.rb
@@ -0,0 +1,18 @@
+module TurtleApp
+  class CommandResolver
+    class UnknownCommandError < StandardError; end
+
+    def call(table:, command:)
+      command_parts = command.split
+      command_name = command_parts[0]
+      command_args = command_parts[1]
+
+      if command_name == 'PLACE'
+        parsed_args = command_args.split(',').map(&:to_i)
+        table.place(parsed_args[0], parsed_args[1])
+      else
+        raise UnknownCommandError, "Unknown command: #{command}"
+      end
+    end
+  end
+end
---
+++ spec/turtle_app/command_resolver_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../lib/turtle_app/command_resolver'
+
+RSpec.describe TurtleApp::CommandResolver do
+  subject { described_class.new }
+
+  let(:table) { instance_double(TurtleApp::Table) }
+
+  describe '#call' do
+    context 'when PLACE command is passed' do
+      it 'calls Table#place method' do
+        expect(table).to receive(:place).with(4, 4)
+
+        subject.call(table: table, command: 'PLACE 4,4')
+      end
+    end
+
+    context 'when unknown command is passed' do
+      it 'raises UnknownCommandError' do
+        expect { subject.call(table: table, command: 'FOO 4,4') }
+          .to raise_error(TurtleApp::CommandResolver::UnknownCommandError, 'Unknown command: FOO 4,4')
+      end
+    end
+  end
+end
