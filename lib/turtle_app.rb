62aa63fb6c207ab179e02987c533a1ed3fa5c628
Mateusz Zdanowicz <zdanowicz.mateusz@gmail.com>
2020-04-15 14:52:01 +0200

    Create TurtleApp start method

--- lib/turtle_app.rb
+++ lib/turtle_app.rb
@@ -1,3 +1,35 @@
+require_relative 'turtle_app/table'
+
 module TurtleApp
   class Error < StandardError; end
+
+  def self.start
+    table = Table.new
+
+    while true
+      system('clear')
+
+      table.place(1, 4)
+      render_to_console(table)
+
+      command = gets.chomp
+      exit(0) if command.empty?
+    end
+  end
+
+  def self.render_to_console(table)
+    puts '_______'
+    table.fields.each do |row|
+      print '|'
+      row.each do |field|
+        if field == 'T'
+          print 'T'
+        else
+          print ' '
+        end
+      end
+      puts '|'
+    end
+    puts '⎻⎻⎻⎻⎻⎻⎻'
+  end
 end
